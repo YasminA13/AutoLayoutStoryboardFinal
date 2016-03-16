7ee80e78ca84b1c154c26ebcefa1f69d0726a864
Yasmin <codeyasmin@gmail.com>
2016-03-15 20:16:10 -0400

    storyboard

---
+++ AutoLayoutStoryboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutStoryboard
+//
+//  Created by Yasmin Ahmad on 2016-03-15.
+//  Copyright © 2016 Yasmin Ahmad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
